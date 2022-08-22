f643e1aa6c9636b5078f0757608326ff7a13c260
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2022-08-22 23:11:51 +0530

    Responsive for header and navbar

--- js/script.js
+++ js/script.js
@@ -3,15 +3,25 @@ let searchBar = document.querySelector('.search-bar-container');
 let formBtn = document.querySelector('#login-btn');
 let loginForm = document.querySelector('.login-form-container');
 let formClose = document.querySelector('#form-close');
+let menu = document.querySelector('#menu-bar');
+let navbar = document.querySelector('.navbar');
 
 window.onscroll = () =>{
     searchBtn.classList.remove('fa-times');
     searchBar.classList.remove('active');
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active');
+    loginForm.classList.remove('active');
 }
 
 searchBtn.addEventListener('click', () =>{
     searchBtn.classList.toggle('fa-times');
     searchBar.classList.toggle('active');
+});
+
+menu.addEventListener('click', () =>{
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
 });
 
 formBtn.addEventListener('click', () =>{
