b618aeff21a8275800b79c9abc11bb5161362847
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2022-08-22 22:36:18 +0530

    Login screen done

--- js/script.js
+++ js/script.js
@@ -1,5 +1,8 @@
 let searchBtn = document.querySelector('#search-btn');
 let searchBar = document.querySelector('.search-bar-container');
+let formBtn = document.querySelector('#login-btn');
+let loginForm = document.querySelector('.login-form-container');
+let formClose = document.querySelector('#form-close');
 
 window.onscroll = () =>{
     searchBtn.classList.remove('fa-times');
@@ -9,5 +12,13 @@ window.onscroll = () =>{
 searchBtn.addEventListener('click', () =>{
     searchBtn.classList.toggle('fa-times');
     searchBar.classList.toggle('active');
+});
+
+formBtn.addEventListener('click', () =>{
+    loginForm.classList.add('active');
+});
+
+formClose.addEventListener('click', () =>{
+    loginForm.classList.remove('active');
 });
 
