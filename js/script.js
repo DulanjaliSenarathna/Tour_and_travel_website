ac99864d3afdc6c50d6973c312c9f6121956f57f
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2022-08-24 20:26:31 +0530

    review section done

--- js/script.js
+++ js/script.js
@@ -42,3 +42,24 @@ videoBtn.forEach(btn =>{
     });
 });
 
+var swiper = new Swiper(".review-slider", {
+    spaceBetween:20,
+    loop:true,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+    breakpoints:{
+        640:{
+            slidesPerView: 1,
+        },
+        768:{
+            slidesPerView: 2,
+        },
+        1024:{
+            slidesPerView: 3,
+        },
+    }
+});
+
+
