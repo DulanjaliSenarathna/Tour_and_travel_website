64cf8e3a579eee9e4caae8c869ef98079bb1c652
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2022-08-24 21:48:57 +0530

    brand section done

--- js/script.js
+++ js/script.js
@@ -62,4 +62,27 @@ var swiper = new Swiper(".review-slider", {
     }
 });
 
+var swiper = new Swiper(".brand-slider", {
+    spaceBetween:20,
+    loop:true,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+    breakpoints:{
+        450:{
+            slidesPerView: 2,
+        },
+        768:{
+            slidesPerView: 3,
+        },
+        991:{
+            slidesPerView: 4,
+        },
+        1200:{
+            slidesPerView: 5,
+        },
+    }
+});
+
 
